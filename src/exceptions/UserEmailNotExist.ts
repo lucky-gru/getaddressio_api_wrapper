925b2cd6006c7298c3562cd3f83470f8e20a8596
rockstar2020 <dev.rockstar2020@gmail.com>
2022-07-19 05:04:43 -0500

    remove useless code

--- src/exceptions/UserEmailNotExist.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from './HttpException';
-
-class UserEmailNotExistsException extends HttpException {
-  constructor(email: string) {
-    super(404, `User with email ${email} doesn't exist`);
-  }
-}
-
-export default UserEmailNotExistsException;
